452657e89ce21f64448aff6e6d2069c2667220c6
SyedMustafaImam <cs1812134@szabist.pk>
2021-03-31 17:00:41 +0500

    Now working fine

--- practice/academicsstudents/mywebsite/controller/students.controller.js
+++ practice/academicsstudents/mywebsite/controller/students.controller.js
@@ -1,4 +1,8 @@
 const student=require('../model/student.model')
+const express = require('express');
+let app = express();
+let router = express.Router();
+ 
 exports.details=(req,res)=>{
     student.findById({courseid:req.params.id},(err,doc)=>{
         if(err) return res.render(err)
@@ -13,4 +17,22 @@ exports.list=(req,res)=>{
            console.log('Error in retrieving Products'+JSON.stringify(err,undefined,2))
         }
     })
+}
+
+exports.show = (req,res)=>{
+    console.log(':id>>',req.params.id)
+    student.findOne({courseid:req.params.id}).then(function(results){
+            res.render('show', {
+              page:"Course Details",
+              menuId:"Details",
+              course:results
+            })
+
+
+    }).catch(function(err){
+        console.log(err)
+    })
+        
+      
+
 }
