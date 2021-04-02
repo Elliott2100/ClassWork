bbc7d7f0a1e77ff4c303c74cb6398c6a1535723f
Elliott Francis Joseph <73632496+Elliott2100@users.noreply.github.com>
2021-04-03 01:03:26 +0500

    Update student.model.js

--- studentapi/model/student.model.js
+++ studentapi/model/student.model.js
@@ -2,9 +2,9 @@ const mongoose=require('mongoose')
 const schema=mongoose.Schema
 let studentschem= new schema({
       name:{type:String, required:true},
-      reg_no:{type:Number,required:true, max:1000000000},
+      reg_no:{type:Number,required:true},
       gender:{type:String, required:true},
       email:{type:String,required:true},
-      CNIC:{type:Number,required:true,max:99999999}
+      CNIC:{type:Number,required:true}
 })
 module.exports= mongoose.model('Students',studentschem)
